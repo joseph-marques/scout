e6f3873a616c0e12358d36dd94b16543c8fb42e6
Joseph Marques <jlm123hi@gmail.com>
2020-02-20 14:30:59 -0600

    Generate binary data from schema files.

--- cloud_functions/functions/init.go
+++ cloud_functions/functions/init.go
@@ -2,17 +2,13 @@ package scout
 
 import (
 	graphql "github.com/graph-gophers/graphql-go"
-        "github.com/graph-gophers/graphql-go/relay"
+	"github.com/graph-gophers/graphql-go/relay"
 )
 
 var h *relay.Handler
 
 func init() {
-	s := `
-                type Query {
-                        hello: String!
-                }
-        `
-        schema := graphql.MustParseSchema(s, &query{})
-        h = &relay.Handler{Schema: schema}
+	s := SchemaString()
+	schema := graphql.MustParseSchema(s, &query{})
+	h = &relay.Handler{Schema: schema}
 }
