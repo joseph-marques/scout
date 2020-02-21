8acaea9d65f02f970a2e9adbc30e8978dac6f96f
Carter Perrin <carterperrin@icloud.com>
2020-02-20 19:19:20 -0600

    Updates

---
+++ precache-manifest.5fd93c7b003a3fe3eac2ead19340d03b.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f017717ad09d3f7e2acb07302d5d95c",
+    "url": "/joseph-marques/scout/index.html"
+  },
+  {
+    "revision": "88ccb40ddba31f4bafb6",
+    "url": "/joseph-marques/scout/static/css/2.6b1c2b0c.chunk.css"
+  },
+  {
+    "revision": "b1c2b4ab4db68a374df2",
+    "url": "/joseph-marques/scout/static/css/main.83228805.chunk.css"
+  },
+  {
+    "revision": "88ccb40ddba31f4bafb6",
+    "url": "/joseph-marques/scout/static/js/2.6c3ae806.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/joseph-marques/scout/static/js/2.6c3ae806.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1c2b4ab4db68a374df2",
+    "url": "/joseph-marques/scout/static/js/main.9eb7da70.chunk.js"
+  },
+  {
+    "revision": "37dd541fe17fb973ef99",
+    "url": "/joseph-marques/scout/static/js/runtime-main.06d6d776.js"
+  },
+  {
+    "revision": "a39fead67118632a04c96b35bc469b4f",
+    "url": "/joseph-marques/scout/static/media/fredoka-one-latin-400.a39fead6.woff"
+  },
+  {
+    "revision": "e12e0aa363a60ce6f2140ba7565cadf6",
+    "url": "/joseph-marques/scout/static/media/fredoka-one-latin-400.e12e0aa3.woff2"
+  },
+  {
+    "revision": "24f7b0944e9e03a905f9d7701573b2cd",
+    "url": "/joseph-marques/scout/static/media/open-sans-latin-300.24f7b094.woff2"
+  },
+  {
+    "revision": "a7622f60c56ddd5301549a786b54e6e6",
+    "url": "/joseph-marques/scout/static/media/open-sans-latin-300.a7622f60.woff"
+  },
+  {
+    "revision": "6725c3fb1f3abd7ebdcb2d244758f706",
+    "url": "/joseph-marques/scout/static/media/open-sans-latin-300italic.6725c3fb.woff2"
+  },
+  {
+    "revision": "793b1237017aeacd646fb80911425566",
+    "url": "/joseph-marques/scout/static/media/open-sans-latin-300italic.793b1237.woff"
+  },
+  {
+    "revision": "33543c5cc5d88f5695dd08c87d280dfd",
+    "url": "/joseph-marques/scout/static/media/open-sans-latin-400.33543c5c.woff2"
+  },
+  {
+    "revision": "de0869e324680c99efa1250515b4b41c",
+    "url": "/joseph-marques/scout/static/media/open-sans-latin-400.de0869e3.woff"
+  },
+  {
+    "revision": "06b4bfda4e139eaf3ab9872a6d66f42f",
+    "url": "/joseph-marques/scout/static/media/open-sans-latin-400italic.06b4bfda.woff"
+  },
+  {
+    "revision": "b61a9055d92ac56c15cacf4271d4f072",
+    "url": "/joseph-marques/scout/static/media/open-sans-latin-400italic.b61a9055.woff2"
+  },
+  {
+    "revision": "449d681cd6006390e1bee3c3a660430b",
+    "url": "/joseph-marques/scout/static/media/open-sans-latin-600.449d681c.woff"
+  },
+  {
+    "revision": "819af3d3abdc9f135d49b80a91e2ff4c",
+    "url": "/joseph-marques/scout/static/media/open-sans-latin-600.819af3d3.woff2"
+  },
+  {
+    "revision": "56e5756b696615d6164a625e1bcb1a9e",
+    "url": "/joseph-marques/scout/static/media/open-sans-latin-600italic.56e5756b.woff"
+  },
+  {
+    "revision": "ebdfa24cb411ba3016abd1c3ea5746e5",
+    "url": "/joseph-marques/scout/static/media/open-sans-latin-600italic.ebdfa24c.woff2"
+  },
+  {
+    "revision": "0edb76284a7a0f8db4665b560ee2b48f",
+    "url": "/joseph-marques/scout/static/media/open-sans-latin-700.0edb7628.woff2"
+  },
+  {
+    "revision": "1f85e92d8ff443980bc0f83ad7b23b60",
+    "url": "/joseph-marques/scout/static/media/open-sans-latin-700.1f85e92d.woff"
+  },
+  {
+    "revision": "a49393f2171a2f4d6927ce6c14f73dbc",
+    "url": "/joseph-marques/scout/static/media/open-sans-latin-700italic.a49393f2.woff2"
+  },
+  {
+    "revision": "bf72679ca22e53320beaea090e8bb07d",
+    "url": "/joseph-marques/scout/static/media/open-sans-latin-700italic.bf72679c.woff"
+  },
+  {
+    "revision": "05ebdbe10796850f045fcd484f35788d",
+    "url": "/joseph-marques/scout/static/media/open-sans-latin-800.05ebdbe1.woff"
+  },
+  {
+    "revision": "ab3e500e7375695d702cff19513e4470",
+    "url": "/joseph-marques/scout/static/media/open-sans-latin-800.ab3e500e.woff2"
+  },
+  {
+    "revision": "92da6f116d973bd334cf9b3afdb29c4f",
+    "url": "/joseph-marques/scout/static/media/open-sans-latin-800italic.92da6f11.woff"
+  },
+  {
+    "revision": "c62c3eea77cf3fca51494e8c924adf4d",
+    "url": "/joseph-marques/scout/static/media/open-sans-latin-800italic.c62c3eea.woff2"
+  },
+  {
+    "revision": "0844b197c4f18213fc3a97e703f1acd8",
+    "url": "/joseph-marques/scout/static/media/settings-5.0844b197.svg"
+  },
+  {
+    "revision": "d072b762de234e54b759b3e216a0fb9e",
+    "url": "/joseph-marques/scout/static/media/star-1.d072b762.svg"
+  },
+  {
+    "revision": "354e1a45105ea2fce65f4e4ce983028e",
+    "url": "/joseph-marques/scout/static/media/user-4.354e1a45.svg"
+  },
+  {
+    "revision": "bb1e61c712ed8b63ee9e005e4e4b4779",
+    "url": "/joseph-marques/scout/static/media/user-6.bb1e61c7.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/joseph-marques/scout/precache-manifest.5fd93c7b003a3fe3eac2ead19340d03b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/joseph-marques/scout/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
