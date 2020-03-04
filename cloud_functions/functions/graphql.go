f12f9b6ae815080e04d3fe7a87fe0039ae536602
Joseph Marques <me@josephmarqu.es>
2020-03-04 01:13:13 -0600

    Adding cors to scout-phi.now.sh

--- cloud_functions/functions/graphql.go
+++ cloud_functions/functions/graphql.go
@@ -407,5 +407,16 @@ func (r *resolver) CommentOnAppointment(ctx context.Context, args CommentOnAppoi
 }
 
 func HandleGraphQL(w http.ResponseWriter, r *http.Request) {
+	// Set CORS headers for the preflight request
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "https://scout-phi.now.sh")
+		w.Header().Set("Access-Control-Allow-Methods", "POST")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	// Set CORS headers for the main request.
+	w.Header().Set("Access-Control-Allow-Origin", "https://scout-phi.now.sh")
 	h.ServeHTTP(w, r)
 }
