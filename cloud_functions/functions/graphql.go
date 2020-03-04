186863a55bb88c7dac0b723f025a36bdc7edf928
Joseph Marques <me@josephmarqu.es>
2020-03-03 22:53:53 -0600

    This should now deploy.

--- cloud_functions/functions/graphql.go
+++ cloud_functions/functions/graphql.go
@@ -2,7 +2,9 @@ package scout
 
 import (
 	"context"
+	"fmt"
 	"net/http"
+	"time"
 
 	graphql "github.com/graph-gophers/graphql-go"
 )
@@ -10,21 +12,158 @@ import (
 type resolver struct{}
 
 type scoutResolver struct {
-	ID        graphql.ID       `firebase:"ID"`
-	FirstName string           `firebase:"FirstName"`
-	LastName  string           `firebase:"LastName"`
-	Roles     *[]*roleResolver `firebase:"Roles"`
-	Skills    *[]string        `firebase:"Skills"`
+	ID        graphql.ID          `firebase:"ID"`
+	FirstName string              `firebase:"FirstName"`
+	LastName  string              `firebase:"LastName"`
+	Bio       string              `firebase:"Bio"`
+	Roles     *[]*roleResolver    `firebase:"Roles"`
+	Skills    *[]string           `firebase:"Skills"`
+	IsListed  *bool               `firebase:"IsListed"`
+	Services  *[]*serviceResolver `firebase:"Services"`
 	// rating: ReviewSummary
+}
+
+type roleResolver struct {
+	Title       *string `firebase:"Title"`
+	Institution *string `firebase:"Institution"`
+	Tenure      *string `firebase:"Tenure"`
 }
 
 func (s *scoutResolver) Rating() *reviewSummaryResolver {
 	return nil
 }
 
-type roleResolver struct {
-	Title       *string `firebase:"Title"`
-	Institution *string `firebase:"Institution"`
+func (s *scoutResolver) Reviews(ctx context.Context) (*[]*reviewResolver, error) {
+	reviews, err := db.Collection("Users").Doc(string(s.ID)).Collection("Reviews").Documents(ctx).GetAll()
+	if err != nil {
+		return nil, err
+	}
+
+	resolvers := []*reviewResolver{}
+	for _, review := range reviews {
+		rr := &reviewResolver{}
+		if err := review.DataTo(rr); err != nil {
+			return nil, err
+		}
+		resolvers = append(resolvers, rr)
+	}
+
+	return &resolvers, nil
+}
+
+type reviewResolver struct {
+	AuthorID  graphql.ID `firebase:"Author"`
+	SubjectID graphql.ID `firebase:"Subject"`
+	Rating    float64    `firebase:"Rating"`
+	Text      *string    `firebase:"Tenure"`
+}
+
+func (r *reviewResolver) Author(ctx context.Context) (*scoutResolver, error) {
+	return (&resolver{}).Scout(ctx, ScoutQueryArgs{ID: r.AuthorID})
+}
+
+func (r *reviewResolver) Subject(ctx context.Context) (*scoutResolver, error) {
+	return (&resolver{}).Scout(ctx, ScoutQueryArgs{ID: r.SubjectID})
+}
+
+func (s *scoutResolver) AppointmentsWithMe(ctx context.Context) (*[]*appointmentResolver, error) {
+	reviews, err := db.Collection("Appointments").Where("With", "==", string(s.ID)).Documents(ctx).GetAll()
+	if err != nil {
+		return nil, err
+	}
+
+	resolvers := []*appointmentResolver{}
+	for _, review := range reviews {
+		ar := &appointmentResolver{}
+		if err := review.DataTo(ar); err != nil {
+			return nil, err
+		}
+		timeStr := ar.WhenInternal.Format(time.RFC3339)
+		ar.When = &timeStr
+		resolvers = append(resolvers, ar)
+	}
+
+	return &resolvers, nil
+}
+
+func (s *scoutResolver) AppointmentsWithOthers(ctx context.Context) (*[]*appointmentResolver, error) {
+	reviews, err := db.Collection("Appointments").Where("Requester", "==", string(s.ID)).Documents(ctx).GetAll()
+	if err != nil {
+		return nil, err
+	}
+
+	resolvers := []*appointmentResolver{}
+	for _, review := range reviews {
+		ar := &appointmentResolver{}
+		if err := review.DataTo(ar); err != nil {
+			return nil, err
+		}
+		timeStr := ar.WhenInternal.Format(time.RFC3339)
+		ar.When = &timeStr
+		resolvers = append(resolvers, ar)
+	}
+
+	return &resolvers, nil
+}
+
+type appointmentResolver struct {
+	When         *string
+	WhenInternal *time.Time                     `firebase:"When"`
+	Status       *appointmentStatus             `firebase:"Status"`
+	ServiceID    graphql.ID                     `firebase:"Service"`
+	RequesterID  graphql.ID                     `firebase:"Requester"`
+	WithID       graphql.ID                     `firebase:"With"`
+	Note         *string                        `firebase:"Note"`
+	Comments     *[]*appointmentCommentResolver `firebase:"Comments"`
+}
+
+func (r *appointmentResolver) Requester(ctx context.Context) (*scoutResolver, error) {
+	return (&resolver{}).Scout(ctx, ScoutQueryArgs{ID: r.RequesterID})
+}
+
+func (r *appointmentResolver) With(ctx context.Context) (*scoutResolver, error) {
+	return (&resolver{}).Scout(ctx, ScoutQueryArgs{ID: r.WithID})
+}
+
+func (r *appointmentResolver) Service(ctx context.Context) (*serviceResolver, error) {
+	s, err := (&resolver{}).Scout(ctx, ScoutQueryArgs{ID: r.WithID})
+	if err != nil {
+		return nil, err
+	} else if s.Services == nil {
+		return nil, fmt.Errorf("no service with id %q found", r.ServiceID)
+	}
+
+	for _, service := range *s.Services {
+		if service != nil && service.ID == r.ServiceID {
+			return service, nil
+		}
+	}
+	return nil, fmt.Errorf("no service with id %q found", r.ServiceID)
+}
+
+type appointmentStatus int64
+
+const (
+	statusUnknown appointmentStatus = iota
+	statusRequested
+	statusConfirmed
+	statusPast
+)
+
+type appointmentCommentResolver struct {
+	AuthorID graphql.ID `firebase:"Author"`
+	Comment  string     `firebase:"Comment"`
+}
+
+func (r *appointmentCommentResolver) Author(ctx context.Context) (*scoutResolver, error) {
+	return (&resolver{}).Scout(ctx, ScoutQueryArgs{ID: r.AuthorID})
+}
+
+type serviceResolver struct {
+	ID          graphql.ID `firebase:"ID"`
+	Title       *string    `firebase:"Title"`
+	Description *string    `firebase:"Description"`
+	Price       *string    `firebase:"Price"`
 }
 
 type reviewSummaryResolver struct {
@@ -111,14 +250,25 @@ func (r *resolver) Scouts(ctx context.Context, args ScoutsQueryArgs) (*[]*scoutR
 type RoleInput struct {
 	Title       *string `firebase:"Title"`
 	Institution *string `firebase:"Institution"`
+	Tenure      *string `firebase:"Tenure"`
+}
+
+type ServiceInput struct {
+	ID          graphql.ID `firebase:"ID"`
+	Title       *string    `firebase:"Title"`
+	Description *string    `firebase:"Description"`
+	Price       *string    `firebase:"Price"`
 }
 
 type ScoutInput struct {
-	ID        graphql.ID   `firebase:"ID"`
-	FirstName string       `firebase:"FirstName"`
-	LastName  string       `firebase:"LastName"`
-	Roles     *[]RoleInput `firebase:"Roles"`
-	Skills    *[]string    `firebase:"Skills"`
+	ID        graphql.ID      `firebase:"ID"`
+	FirstName string          `firebase:"FirstName"`
+	LastName  string          `firebase:"LastName"`
+	Bio       string          `firebase:"Bio"`
+	Roles     *[]RoleInput    `firebase:"Roles"`
+	Skills    *[]string       `firebase:"Skills"`
+	Services  *[]ServiceInput `firebase:"Services"`
+	IsListed  *bool           `firebase:"IsListed"`
 }
 
 // UpdateScoutQueryArgs are the arguments for the "scout" query.
